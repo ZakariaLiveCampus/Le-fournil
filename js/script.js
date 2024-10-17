fb2c9f0f4f3bc64eb863cc624a94ac6f10b38f49
ZakariaLiveCampus <zakaria.saber@livecampus.me>
2024-10-17 14:45:14 +0200

    navbar

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Mobile menu 
+const mobileButton = document.getElementById("mobile-button");
+
+const mainNav = document.getElementById("main-nav");
+
+const mobileIcon = document.getElementById("mobile-icon");
+
+function toggleBurger() {
+    if (mobileIcon.classList.contains("fa-bars")) {
+        mobileIcon.classList.replace("fa-bars", "fa-chevron-up");
+    } else {
+        mobileIcon.classList.replace("fa-chevron-up", "fa-bars");
+    }
+}
+
+function toggleNav(event) {
+    if (window.innerWidth >= 768) return;
+    mainNav.classList.toggle("display");
+    document.body.classList.toggle("overflow");
+    toggleBurger();
+}
+
+function resetNav() {
+    mainNav.classList.remove("display");
+    document.body.classList.remove("overflow");
+    mobileIcon.classList.replace("fa-chevron-up", "fa-bars");
+}
+
+mobileButton.addEventListener("click", toggleNav);
+
+mainNav.addEventListener("click", function(event) {
+    if (event.target.hasAttribute("href")) toggleNav();
+})
+
+window.addEventListener("resize", function(event) {
+    if (window.innerWidth >= 768) resetNav();
+})
